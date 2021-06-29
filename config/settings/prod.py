4a6edd772aa6f355714685e794db4d0f906c72a4
surajmt8848 <suraj8848mt@gmail.com>
2021-06-29 13:50:15 +0545

    changes

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -13,32 +13,6 @@ DATABASES = {
     }
 }
 
-# for s3 bucket
-AWS_ACCESS_KEY_ID = "AKIA6OWLQSQFTDUDVP5A"
-AWS_SECRET_ACCESS_KEY = "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"
-AWS_STORAGE_BUCKET_NAME = "atithighar"
-AWS_DEFAULT_ACL = 'public-read'
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-
-# s3 static settings
-STATIC_LOCATION = 'static'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
-STATICFILES_STORAGE = 'atithighar.backends.StaticStorage'
-
-# s3 public media settings
-MEDIA_LOCATION = 'media'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIA_LOCATION}/'
-DEFAULT_FILE_STORAGE = 'atithighar.backends.MediaStorage'
-
-# ses email settings
-AWS_SES_ACCESS_KEY_ID = "AKIA6OWLQSQFQ2DQOYN6"
-AWS_SES_SECRET_ACCESS_KEY = "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"
-
-AWS_SES_REGION_NAME = 'ap-south-1'
-AWS_SES_REGION_ENDPOINT = f'email.{AWS_SES_REGION_NAME}.amazonaws.com'
-
-EMAIL_BACKEND = 'django_ses.SESBackend'
 
 # production settings
 CORS_REPLACE_HTTPS_REFERER = True
